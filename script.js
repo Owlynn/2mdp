ccc07c7675f8ea6ce20a32397bcb85320dd90ab6
Owlynn <contact@jessalynn.fr>
2020-10-24 23:37:35 +0200

    Implémentation du template avec fonction play et modif du css

--- script.js
+++ script.js
@@ -1,61 +1,23 @@
 // HANDLEBARS CONTEXT 
-const context = {
-    titre:'test',
-    samples: [
-        {
-          title: 'Kapoué',
-          url: 'https://www.donnerdelavoix.fr/test/test.mp3',
-          
-        },
-        {
-          title: 'Columbo',
-          url: 'https://www.donnerdelavoix.fr/test/test.mp3',
-          
-        },
-        {
-          title: 'Qui va se ',
-          url: 'https://www.donnerdelavoix.fr/test/test.mp3',
-          
-        },
-        {
-          title: 'Une ligne',
-          url: 'https://www.donnerdelavoix.fr/test/test.mp3',
-          
-        },
-        {
-          title: 'one line',
-          url: 'https://www.donnerdelavoix.fr/test/test.mp3',
-          
-        } 
-    ]
-};
 
- // HANDLEBARS SETUP
-
+// HANDLEBARS SETUP
 const templateElement = document.getElementById('hb');
 const templateSource = document.getElementById('hb').innerHTML;
 const template = Handlebars.compile(templateSource);
-const compiledHtml= template(context);
+const compiledHtml= template({samples});
 
-    // BLOCS DE SAMPLES VIA HANDLEBARS
-
+// BLOCS DE SAMPLES VIA HANDLEBARS
 document.getElementById('blocs').innerHTML = compiledHtml;
 
 // BOUTONS INTERACTIFS
-console.log(document.getElementsByClassName("trigger"));
-const pwet =  function(event) {alert ("pwet")} ;
+// console.log(document.getElementsByClassName("trigger"));
 
-let eventTarget = document.getElementById("bouton"); //premier bloc de la liste uniquement
-let eventClass = document.getElementsByClassName("trigger"); // tous les blocs/boutons dans un tableau
+const boutons = Array.from(document.getElementsByClassName("boutons"));
 
+boutons.forEach ( function (currentButton){
+  currentButton.onclick = (event) => {
+    const playerId=`${event.target.id}-player`;
+    document.getElementById(playerId).play();
+  }
 
-eventTarget.onclick = pwet ; //fonctionne Cas 1
-const test = function (elem) {
-  console.log(elem);
-  elem.onclick = pwet;
-
-} ;
-
-Array.from(eventClass).forEach(test); // convertit en vrai tableau
-
-
+})
